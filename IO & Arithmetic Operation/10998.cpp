f77776e348866afa45613915e5c990b9db48d1e9
Nexy <92451281+DevNexy@users.noreply.github.com>
2022-08-02 03:36:32 +0900

    Add files via upload

---
+++ IO & Arithmetic Operation/10998.cpp
@@ -0,0 +1,13 @@
+//두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int c;
+    c=a*b;
+    cout<<c<<endl;
+    return 0;
+}
